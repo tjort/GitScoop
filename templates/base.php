34bb6002eb742945c8ce5d7287bcf634778d9813
Martijn Gussekloo <gus@stylishmedia.com>
2013-05-09 17:21:10 +0200

    Worked on template

--- templates/base.php
+++ templates/base.php
@@ -9,27 +9,27 @@
 	<link rel="stylesheet" type="text/css" href="/assets/kube/kube.min.css" />
 	<link rel="stylesheet" type="text/css" href="/assets/ghl/ghl.css" />
 	<link rel="stylesheet" type="text/css" href="/assets/source-sans/style.css" />
+	<link rel="stylesheet" type="text/css" href="/assets/icomoon/style.css" />
 </head>
 <body>
 	<div id="header">
 		<div class="page">
 			<div class="padded">
 				<div class="row">
-					<h1>GitScoop</h1>
+					<h1><a href="/"><i class="icon-eye"></i> GitScoop</a></h1>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div id="banner">
-
 		<div class="page">
 			<div class="padded">
 				<div class="row">
 					<div id="search" class="two thirds">
 						<div class="padded">
 							<form>
-								<p>What is <input type="text" placeholder="Github user" /> secretly working on? <input type="submit" value="Find out!" /></p>
+								<p>What is <input type="text" placeholder="Github user" autofocus="true" /> secretly working on? <input type="submit" value="Find out!" /></p>
 							</form>
 						</div>
 					</div>
@@ -38,8 +38,6 @@
 		</div>
 	</div>
 
-
-
 	<div id="colofon">
 		<div class="page">
 			<div class="row">
@@ -47,18 +45,40 @@
 					<div class="padded">
 						<h2>What's this?</h2>
 						<p>
-							GitScoop uses the GitHub-API to investigate the various projects in a users activity-stream. Then, amazingly, it deduces what this user is probably coding, forking or compiling. And if he or she isn't, he or she probably should. Because that's the scoop.
+							GitScoop uses the GitHub API to investigate the various projects in a users activity-stream. Then, amazingly, it deduces what this user is probably coding, forking or compiling. And if he or she isn't, he or she probably should. Because that's the scoop.
 						</p>
 					</div>
 				</div>
 				<div class="third">
 					<div class="padded">
 						<h2>Who made this?</h2>
+						<p>
+							Martijn (who works at <a href="">Stylishmedia</a>) created this app with help from the Github API, Slim PHP Framework, Guzzler, Kube CSS and various others.
+						</p>
+						<br /><br />
+						<p>
+							<ul class="social">
+							    <li><a href="http://www.github.com/mgussekloo"><em class="icon-github"></em> <span>Github</span></a></li>
+							    <li><a href="http://www.twitter.com/mgussekloo"><em class="icon-twitter"></em> <span>Twitter</span></a></li>
+							    <li><a href="http://www.linkedin.com/mgussekloo"><em class="icon-linkedin"></em> <span>LinkedIn</span></a></li>
+							</ul>
+						</p>
 					</div>
 				</div>
 				<div class="third">
 					<div class="padded">
-						<h2>Latest GitScoops</h2>
+						<h2>Latest</h2>
+						<dl>
+							<dt>schmkr</dt>
+							<dd>is probably working on Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</dd>
+							<dt>nickdunn</dt>
+							<dd>is probably working on Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</dd>
+							<dt>joel spolsky</dt>
+							<dd><dd>is probably working on Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+							tempor incididunt ut labore et dolore magna aliqua.</dd></dd>
+						</dl>
 					</div>
 				</div>
 			</div>
