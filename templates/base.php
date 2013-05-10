dbca292d2832808fd888911cf8952b53081f9f8f
Martijn Gussekloo <gus@stylishmedia.com>
2013-05-10 18:47:23 +0200

    Title

--- templates/base.php
+++ templates/base.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>GitHub leaks</title>
+	<title>GitScoop - we scoop what you fork</title>
 
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
